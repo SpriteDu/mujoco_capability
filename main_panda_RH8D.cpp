d9c87ab367136e32d86cdc5d0105cf9779a157ca
Jididu Li <jididu.li@tum.de>
2023-11-16 08:21:38 -0800

    outer controller

--- main_panda_RH8D.cpp
+++ main_panda_RH8D.cpp
@@ -96,39 +96,10 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset)
     mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn, &cam);
 }
 
-
-void controlLaw(const mjModel* m, mjData* d)
+void controlSystem(const mjModel* m, mjData* d, double ref[])
 {
-    //any++;
-    int n_joints = m->nq;
-    double ref[n_joints] = {1.90973, // Panda DOF 1
-                      1.21453, // Panda DOF 2
-                      0.536998, // Panda DOF 3
-                      -2.61271, // Panda DOF 4
-                      -0.849991, // Panda DOF 5
-                      1.54611, // Panda DOF 6
-                      1.57, // Panda DOF 7
-                      0, // Forearm --> Should be kept in 0 as it is not actuated
-                      0, // Wrist adduction/abduction
-                      0, // Wrist Flexion/Extension
-                      1.5, // Thumb MCP Adduction
-                      0, // Thumb MCP Flexion
-                      0, // Thumb PIP Flexion
-                      0, // Thumn DIP Flexion
-                      1.5, // Index MCP Flexion
-                      0, // Index PIP Flexion
-                      0, // Index DIP Flexion
-                      1.5, // Middle MCP Flexion
-                      0, // MIddle PIP Flexion
-                      0, // MIddle DIP Flexion
-                      1.5, // Ring MCP flexion
-                      0, // Ring PIP Flexion
-                      0, // Ring DIP Flexion
-                      1.5, // Little MCP Flexion
-                      0, // Little PIP Flexion
-                      0}; // Little DIP Flexion
-
-    float kp = 0.008f;
+    int n_joints = m->nu;
+    float kp = 0.013f;
     double err = 0.0;
     for(int i = 0; i < n_joints; i++)
     {
@@ -137,16 +108,15 @@ void controlLaw(const mjModel* m, mjData* d)
     }
 }
 
-void controlSystem(const mjModel* m, mjData* d, double ref[])
+void pcontroller(const mjModel* m, mjData* d)
 {
-    int n_joints = m->nq;
-    float kp = 0.005f;
-    double err = 0.0;
-    for(int i = 0; i < n_joints; i++)
-    {
-        err = ref[i] - d->qpos[i];
-        d->ctrl[i] = d->ctrl[i] + kp*(err);
-    }
+  cout << m->nu << m->nv <<endl;
+  if( m->nu==m->nv )
+  std::cout << "equal"<<endl;
+  double ref[] = {0.470887, 0.978366, 2.6895, -2.15875, 2.75034, 1.88727, 0.707};
+
+    mju_scl3(d->ctrl, d->qvel, -0.1);
+
 }
 
 void printSensorData(const mjModel* m, mjData* d, bool posvel)
@@ -245,10 +215,10 @@ int main(int argc, const char** argv)
     for (auto i = 0; i < 7; i++)
         q_actual_array[i] = d->qpos[i];
 
-    O_T_EE_array[0] = 0; O_T_EE_array[4] = 0; O_T_EE_array[8] = -1;    O_T_EE_array[12] = -0.2;
-    O_T_EE_array[1] = 1; O_T_EE_array[5] = 0; O_T_EE_array[9] = 0;    O_T_EE_array[13] = 0.05;
-    O_T_EE_array[2] = 0; O_T_EE_array[6] = -1; O_T_EE_array[10] = 0;  O_T_EE_array[14] = 0.50;
-    O_T_EE_array[3] = 0; O_T_EE_array[7] = 0; O_T_EE_array[11] = 0;   O_T_EE_array[15] = 1;       
+    O_T_EE_array[0] = -1; O_T_EE_array[4] = 0; O_T_EE_array[8] = 0;    O_T_EE_array[12] = -0.25;
+    O_T_EE_array[1] = 0; O_T_EE_array[5] = -1; O_T_EE_array[9] = 0;    O_T_EE_array[13] = 0.13;
+    O_T_EE_array[2] = 0; O_T_EE_array[6] = 0; O_T_EE_array[10] = 1;  O_T_EE_array[14] = 1.2;
+    O_T_EE_array[3] = 0; O_T_EE_array[7] = 0; O_T_EE_array[11] = 0;   O_T_EE_array[15] = 1;      
 
     Ik_solution ik_sol;
     ik_sol.define_sol_par(O_T_EE_array, q_actual_array, q7);
@@ -300,57 +270,42 @@ int main(int argc, const char** argv)
     double desired_real_time_factor = 1.0;  // Adjust this factor as needed
     double normal_dt = 1.0 / 60.0;  // 60 Hz
     double dt = normal_dt / desired_real_time_factor;
-    m->opt.timestep = dt;
+    // m->opt.timestep = dt;  // the integral time in each step. should align with the step size defined earlier. 
+    cout<<"the intergal time is " << m->opt.timestep<< endl;
+    
 
-    double start_time = glfwGetTime();
+    // for P controller the calling frequency influence the controled behavior
+    double controlSystem_dt = 1.0 / 60.0;  // 60 Hz for control system
+    double lastControlUpdateTime = 0.0;
+    // mjcb_control = pcontroller;
+    mjtNum start_time = d->time;;
     while( !glfwWindowShouldClose(window) )
     {        
-        double current_time = glfwGetTime();
-        double elapsed_time = current_time - start_time;
 
-        if (elapsed_time >= dt)
-        {
-            // Advance the simulation by one time step
-            mj_step(m, d);
-            // Your rendering or control code here
+        mjtNum simstart = d->time;
+        
+        // while last step is finished and a frame is rendered,, keep do the simulation without re-rendering.
+        while (d->time - simstart < dt) {
+        mj_step(m, d);
+        }
+        
+        mjtNum elapsed_time = d->time - start_time;//set after the while loop to decrease the influence from.
+        //steady the calling frequency of controllSystem.
+        if (elapsed_time - lastControlUpdateTime >= controlSystem_dt){
+            
+            // mj_step(m,d);
+            // mj_step1(m, d);
             controlSystem(m, d, ref);
-            //printSensorData(m, d, 0);
-            start_time = current_time;
+            // mj_step2(m,d);
+             lastControlUpdateTime = elapsed_time;
         }
-
-        //std::cout << "Elapsed time: " << d->time << std::endl;
-        // Maintain a consistent frame rate using sleep
-        double sleep_time = dt - (glfwGetTime() - start_time);
-        if (sleep_time > 0)
-        {
-            // Sleep to control the frame rate
-            // Platform-specific sleep or delay function
-            std::this_thread::sleep_for(std::chrono::duration<double>(sleep_time));
-        }
-
-        /*ncon = d->ncon; // Number of contacts
-        std::cout << ncon << "/ " << d->contact[0].pos[0] << "/ " << d->contact[0].pos[1] << "/ " << d->contact[0].pos[2] <<
-                             "/ " << d->contact[0].frame[0] << "/ " << d->contact[0].frame[1] << "/ " << d->contact[0].frame[2] <<
-                             "/ " << d->contact[0].mu << "/ " << d->contact[0].geom1 <<  "/ " << d->contact[0].geom2
-                << std::endl;*/
+       
+        
 
 
-        // advance interactive simulation for 1/60 sec
-        //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
-        //  this loop will finish on time for the next frame to be rendered at 60 fps.
-        //  Otherwise add a cpu timer and exit this loop when it is time to render.
-
-
-        //m->opt.timestep *= time_scale;
-        //mjtNum simstart = d->time;
-        //while( d->time - simstart < 1.0/60.0 )
-        //    mj_step(m, d);
-        /////////////////////////
-        //controlSystem(m, d, ref);
-
-
-        //printf("{%f, %f, %f, %f, %f, %f};\n",cam.azimuth,cam.elevation, cam.distance,cam.lookat[0],cam.lookat[1],cam.lookat[2]);
-
+        // get framebuffer viewport
+        mjrRect viewport = {0, 0, 0, 0};
+        glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
 
         // update scene and render
         mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
@@ -361,11 +316,8 @@ int main(int argc, const char** argv)
 
         // process pending GUI events, call GLFW callbacks
         glfwPollEvents();
-
-
-
-
-    }
+    
+    } // end simulation
 
     //free visualization storage
     mjv_freeScene(&scn);
@@ -382,3 +334,24 @@ int main(int argc, const char** argv)
     //delete[] corr_sys;
     return 1;
 }
+
+//         // // Maintain a consistent frame rate using sleep
+//         // double sleep_time = dt - (glfwGetTime() - start_time);
+//         // if (sleep_time > 0)
+//         // {
+//         //     // Sleep to control the frame rate
+//         //     // Platform-specific sleep or delay function
+//         //     std::this_thread::sleep_for(std::chrono::duration<double>(sleep_time));
+//         // }
+
+//         /*ncon = d->ncon; // Number of contacts
+//         std::cout << ncon << "/ " << d->contact[0].pos[0] << "/ " << d->contact[0].pos[1] << "/ " << d->contact[0].pos[2] <<
+//                              "/ " << d->contact[0].frame[0] << "/ " << d->contact[0].frame[1] << "/ " << d->contact[0].frame[2] <<
+//                              "/ " << d->contact[0].mu << "/ " << d->contact[0].geom1 <<  "/ " << d->contact[0].geom2
+//                 << std::endl;*/
+
+
+
+
+//         //printf("{%f, %f, %f, %f, %f, %f};\n",cam.azimuth,cam.elevation, cam.distance,cam.lookat[0],cam.lookat[1],cam.lookat[2]);
+
