d6d358a29b1311756b93ff918e4eef2276da72b7
Jididu Li <jididu.li@tum.de>
2024-01-10 22:28:44 +0100

    commend line for csv file name

--- src/simple_plot.py
+++ src/simple_plot.py
@@ -1,32 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# Sample data (replace this with the path to your dataset file)
-
-
-
+import sys
 import os
 
-file_path = "build/New_Record.csv"
-if os.path.exists(file_path):
-    print("The file exists")
-else:
-    print("The file does not exist")
+file_path = "../build/New_Record.csv"
 
+# Check if the file name is provided as a command-line argument
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+    file_path = f"../build/{file_name}"
+
+    if os.path.exists(file_path):
+        print("The file exists")
+        # Add your data processing code here
+    else:
+        print("The file does not exist")
 
 data = pd.read_csv(file_path)
 
-df = pd.read_csv(file_path, skiprows=1)# Assigning names to the columns
+df = pd.read_csv(file_path, skiprows=2)# Assigning names to the columns
 
-df.columns = ['Source', 'Time', 'Force1', 'Force2', 'Force3', 'Force4', 'Force5', 'Force6']
+df.columns = ['Source', 'Time', 'Force1', 'Force2', 'Force3'] #,'Force4', 'Force5', 'Force6'
 unique_sources = df['Source'].unique()
 
 # Plotting
-fig, axs = plt.subplots(6, 1, figsize=(10, 15))  # 6 rows, 1 column
+fig, axs = plt.subplots(3, 1, figsize=(10, 15))  # 6 rows, 1 column
 fig.tight_layout(pad=3.0)
 
 # Loop through each force column
-for i in range(1, 7):
+for i in range(1, 4):
     for source in unique_sources:
         filtered_df = df[df['Source'] == source]
         # Convert to NumPy arrays before plotting
